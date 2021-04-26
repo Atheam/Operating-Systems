f1071afba96658677ab20b6f986c005e45b658f0
Atheam <adam.dyda1@gmail.com>
2021-04-26 17:10:09 +0200

    added zad2

---
+++ lab5/zad2/README.txt
@@ -0,0 +1,14 @@
+##############################################3
+
+Aby uruchomic program i przetestowac dzialanie nalezy wykonac komende 
+
+	make run-1
+
+dla jednego argumentu (tutaj "nadawca") lub
+
+	make run-3 address=ADRES_EMAIL
+
+dla trzech argumentow (w miejsce ADRES_EMAIL nalezy wpisac adres email)
+
+
+####################################################################
---
+++ lab5/zad2/zad2.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main(int argc, char* argv[]){
+
+
+    if(argc != 2 && argc != 4){
+        perror("WRONG NUMBER OF ARGUMENTS");
+        return -1;
+    }
+
+
+    if(argc == 2){
+        char *to_run;
+        if(strcmp(argv[1],"nadawca") == 0) to_run = "mail -H | sort -k 3";
+        if(strcmp(argv[1],"data") == 0) to_run = "mail -H ";
+
+        FILE* mail_out = popen(to_run,"r");
+
+        char *line = NULL;
+        size_t len = 0;
+        ssize_t read;
+        while((read = getline(&line,&len,mail_out)) != -1) printf("%s",line);
+
+        pclose(mail_out);
+    }
+    if(argc == 4){
+        char *receiver = argv[1];
+        char *topic = argv[2];
+        char *content = argv[3];
+        char to_run[1024];
+        
+        sprintf(to_run,"echo %s | mail -s %s %s",content,topic,receiver);
+        FILE* mail_out = popen(to_run,"r");
+        pclose(mail_out);
+
+
+    }
+
+
+
+
+
+
+
+}
